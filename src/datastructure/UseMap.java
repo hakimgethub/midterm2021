e6b1bb76a8e47acdc90db439ffdeb17d13019957
rabah touahri <rtouahri2000@yahoo.com>
2021-10-19 11:53:00 -0400

    test test

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -19,9 +19,52 @@ public class UseMap {
 
 
         //implementation here
+		ConnectDB ct = new ConnectDB();
+
+		Map<String,String> personalInfo =new HashMap<>();
+
+		personalInfo.put("first name","rabah");
+		personalInfo.put("last name","hakim");
+		personalInfo.put("cellphone","9547086040");
+		personalInfo.put("email address","hakimrabah1979@gmail.com");
+		System.out.println(personalInfo.get("cellphone"));
+		for(Map.Entry entry:personalInfo.entrySet()){
+			System.out.println(entry.getKey()+""+entry.getValue());
+		}
+		List<String> citiesOfalgeria =new ArrayList<>();
+		citiesOfalgeria.add("alg");
+		citiesOfalgeria.add("constantine ");
+		citiesOfalgeria.add("setif");
+
+		List<String> citiesOffrance =new ArrayList<>();
+		citiesOffrance.add("paris");
+		citiesOffrance.add("lyon");
+		citiesOffrance.add("nancy");
+
+		List<String> citiesOfENGLAND =new ArrayList<>();
+		citiesOfENGLAND.add("london");
+		citiesOfENGLAND.add("manchester");
+		citiesOfENGLAND.add("leeds");
+
+		Map<String,List<String>>map=new HashMap<>();
+		map.put("libya",citiesOfalgeria);
+		map.put("france",citiesOffrance);
+		map.put("ENGLAND",citiesOfENGLAND);
+
+		System.out.println(map.get("libya "));
+		for(Map.Entry entry:map.entrySet()){
+			System.out.println(entry.getKey()+""+entry.getValue());
+		}
 
 
-        //connect to database
+
+
+	}
+}
+
+
+
+//connect to database
 
 
     }
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -15,6 +15,47 @@ public class UseQueue {
 
 		//implementation here
 
+
+		public class UseQueue {
+
+			public static void main(String[] args) {
+				/*
+				 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
+				 * Use For Each loop and while loop with Iterator to retrieve data.
+				 *
+				 */
+
+				Queue<String> queue = new LinkedList<>();
+				queue.add("VA");
+				queue.add("CA");
+				queue.add("MD");
+				queue.add("LA");
+				queue.add("PA");
+				queue.add("NY");
+
+
+				System.out.println(queue.peek());
+				System.out.println(queue.remove());
+				System.out.println(queue.peek());
+				System.out.println(queue.poll());
+				System.out.println(queue.peek());
+
+
+				System.out.println("Use of for each loop to retrieve data: ");
+				for (String st : queue) {
+					System.out.println(st);
+				}
+
+				System.out.println("Use of while loop to retrieve data: ");
+				Iterator itr = queue.iterator();
+				while (itr.hasNext()) {
+					String i = (String) itr.next();
+					System.out.println(i);
+				}
+			}
+
+		}
+
 	}
 
 }
