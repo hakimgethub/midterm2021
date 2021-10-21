fa8e9f87e59557ca783e11ed968ccab4dff00baf
rabah touahri <rtouahri2000@yahoo.com>
2021-10-20 23:35:18 -0400

    math

--- src/math/LowestNumber.java
+++ src/math/LowestNumber.java
@@ -16,7 +16,43 @@ public class LowestNumber {
 
 		//implementation here...
 
+		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
+																System.out.println("Lowest number from ");
+		ArrayList<Integer> arrList = new ArrayList<>();
+		arrList.add(LowestNumber.lowestNumber(array));
+
+		List list =  arrList;
+
+		//Connect to MySql Database
+		ConnectToSqlDB connectDB = new ConnectToSqlDB();
+
+		//Create table in the database
+		connectDB.insertDataFromArrayListToSqlTable(list,"lowestNumber","number");
+
+
+		//Read data from database
+		List<String> numbers = connectDB.readDataBase("lowestNumber", "number");
+		for(String st:numbers){
+			System.out.println(st);
+		}
 	}
 
+	public static int lowestNumber(int [] array){
+
+		int lowest = Integer.MAX_VALUE;
+
+		for (int value : array) {
+			if (lowest > value) {
+				lowest = value;
+			}
+		}
+
+		return lowest;
+	}
 }
+
+
+
+
+
